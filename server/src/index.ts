0d9f13ae2197b69d42f57c644ae5cd1d88ee8351
Felipe Bueno <felipebueno181@gmail.com>
2020-06-04 14:16:59 -0300

    [API] Encerrando o backend

--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/users', (request, response) => {
-    response.json(["Diego", "Cleiton", "Robson"]);
-});
-
-app.listen('3333', () => console.log('🚀 server is launched! at port 3333'));
